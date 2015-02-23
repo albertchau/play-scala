28d580d7012735af8b332ff765b45cbffbadc616
ajchau <albertjmchau@gmail.com>
2015-02-22 19:28:17 -0800

    123123 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,26 +15,3 @@ aws elasticbeanstalk create-application-version --application-name "play-app" --
 
 # Deploy the image
 aws elasticbeanstalk update-environment --environment-name playApp-env --version-label $GIT_COMMIT
-
-Alberts-MBP:docker achau$ history 20
-  509  cd ..
-  510   cd..
-  511  ll
-  512  sbt docker:stage
-  513  cd ..
-  514  ll
-  515  sbt docker:stage
-  516  cp Dockerrun.aws.json target/docker
-  517  cd target/docker
-  518  GIT_COMMIT=39jdf94
-  519  zip -r $GIT_COMMIT.zip *
-  520  ll
-  521  rm play-scala.zip 39jdf94.zip
-  522  zip -r $GIT_COMMIT.zip *
-  523  ll
-  524  aws s3api put-object --bucket test-deploy-ebs --key $GIT_COMMIT --body $GIT_COMMIT.zip
-  525  aws elasticbeanstalk create-application-version --application-name "play-app" --version-label $GIT_COMMIT --source-bundle S3Bucket=test-deploy-ebs,S3Key=$GIT_COMMIT
-  526  aws elasticbeanstalk update-environment --environment-name playApp-env --version-label $GIT_COMMIT
-  527  history 10
-  528  history 20
-Alberts-MBP:docker achau$
