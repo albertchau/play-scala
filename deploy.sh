d9d2f772ea7cb21092ca6cb361b7478a7dc6ac30
ajchau <albertjmchau@gmail.com>
2015-02-22 20:17:58 -0800

    123123 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,14 @@ cp Dockerrun.aws.json target/docker
 cd target/docker
 zip -r $GIT_COMMIT.zip *
 
+S3Bucket=elasticbeanstalk-us-west-2-415926896195
+
 # -- AWS --
 # Upload the image
-aws s3api put-object --bucket test-deploy-ebs --key $GIT_COMMIT --body $GIT_COMMIT.zip
+aws s3api put-object --bucket ${S3Bucket} --key $GIT_COMMIT --body $GIT_COMMIT.zip
 
 # Create a new application version
-aws elasticbeanstalk create-application-version --application-name "play-app" --version-label $GIT_COMMIT --source-bundle S3Bucket=test-deploy-ebs,S3Key=$GIT_COMMIT
+aws elasticbeanstalk create-application-version --application-name "play-app" --version-label $GIT_COMMIT --source-bundle S3Bucket=${S3Bucket},S3Key=$GIT_COMMIT
 
 # Deploy the image
 aws elasticbeanstalk update-environment --environment-name playApp-env --version-label $GIT_COMMIT
